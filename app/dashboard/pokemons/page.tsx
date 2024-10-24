29cf088dbceffb299ba0631b7c1e2762ebb33670
mestrada <mestrada@gmail.com>
2024-10-24 15:55:48 +0200

    Dynamic route /name

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,5 +1,11 @@
 import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 
+
+export const metadata = {
+ title: '151 Pokemons',
+ description: '151 Pokemons',
+};
+
 const getPokemons = async( limit = 20, offset = 0 ): Promise<SimplePokemon[]> => {
     const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
         .then( res => res.json());
